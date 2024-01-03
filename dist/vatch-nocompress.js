4626454f7f72cf2cecf3b43d5734475a2279c446
creuser <151720755+creuserr@users.noreply.github.com>
2024-01-04 02:12:31 +0800

    update vatch-nocompress.js

--- dist/vatch-nocompress.js
+++ dist/vatch-nocompress.js
@@ -28,7 +28,7 @@ async function Vatch(key) {
     },
     // set data: 1 request
     async set(name, content) {
-      content = encodeURI(JSON.stringify(content));
+      content = (JSON.stringify(content));
       var reqc = encodeURI(JSON.stringify([{
         tag: "p",
         children: [content]
